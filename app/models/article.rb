903618821e221b53d734541b95d38d4153618260
Stephen Utterback <utterbackstephen@gmail.com>
2020-11-23 09:28:57 -0600

    added article show

---
+++ app/models/article.rb
@@ -0,0 +1,6 @@
+class Article < ApplicationRecord
+# class Article(capitalized) inherited from application_record.rb 
+  
+  validates :title, presence: true, length: { minimum: 6, maximum: 100 }
+  validates :description, presence: true, length: { minimum: 10, maximum: 300 }
+end
