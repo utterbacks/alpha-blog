134de554606062787c148dd82ca77e88f5b0219c
Stephen Utterback <utterbackstephen@gmail.com>
2020-11-24 20:16:49 -0600

    refactored

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,7 +1,7 @@
 class ArticlesController < ApplicationController
+  before_action :set_article, only: [:show, :edit, :update, :destroy]
   def show
     
-    @article = Article.find(params[:id])
   end
   def index
     @articles = Article.all
@@ -12,14 +12,13 @@ class ArticlesController < ApplicationController
   end
 
   def edit
-    @article = Article.find(params[:id])
   end
 
   def create
     # you need to whitelist what comes in through the params
     # this is a security feature to prevent malicious people
     # from crashing your app
-    @article = Article.new(params.require(:article).permit(:title, :description))
+    @article = Article.new(article_params)
     if @article.save
       # notice or alert can go here. Notice is typcally used to tell the user things worked
       flash[:notice] = "Article was created successfully"
@@ -32,8 +31,7 @@ class ArticlesController < ApplicationController
 
 
   def update
-    @article = Article.find(params[:id])
-    if @article.update(params.require(:article).permit(:title, :description))
+    if @article.update(article_params)
       flash[:notice] = "Article was updated"
       redirect_to @article
     else
@@ -42,9 +40,16 @@ class ArticlesController < ApplicationController
   end
 
   def destroy
-    @article = Article.find(params[:id])
     @article.destroy
     redirect_to articles_path
   end
 
+  private
+  def set_article
+    @article = Article.find(params[:id])
+  end
+
+  def article_params
+    params.require(:article).permit(:title, :description)
+  end
 end
