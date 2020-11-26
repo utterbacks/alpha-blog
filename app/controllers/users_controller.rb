f20ed20156558c6021ff8dcc92dbaece01130905
Stephen Utterback <utterbackstephen@gmail.com>
2020-11-26 02:26:24 -0600

    edit users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,6 +3,21 @@ class UsersController < ApplicationController
   def new
     @user = User.new
   end
+
+  def edit
+    @user = User.find(params[:id])
+  end
+
+  def update
+    @user = User.find(params[:id])
+    if @user.update(user_params)
+      flash[:notice] ="Your account information was successfully updated."
+      redirect_to articles_path
+    else
+      render 'edit'
+    end
+  end
+
   def create
     @user = User.new(user_params)
     if @user.save
@@ -12,6 +27,8 @@ class UsersController < ApplicationController
       render 'new'
     end
   end
+
+
   private 
   def user_params
     params.require(:user).permit(:username, :email, :password)
