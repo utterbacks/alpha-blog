bce29e707392dbf96212158e01bfcc44cd51f525
Stephen Utterback <utterbackstephen@gmail.com>
2020-11-27 23:45:27 -0600

    created categories, added a lot of testing.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -20,6 +20,7 @@ class ArticlesController < ApplicationController
     # you need to whitelist what comes in through the params
     # this is a security feature to prevent malicious people
     # from crashing your app
+    byebug
     @article = Article.new(article_params)
     @article.user = current_user
     if @article.save
@@ -52,8 +53,10 @@ class ArticlesController < ApplicationController
     @article = Article.find(params[:id])
   end
 
+# this is the whitelisting method
+
   def article_params
-    params.require(:article).permit(:title, :description)
+    params.require(:article).permit(:title, :description, category_ids: [])
   end
 
   def require_same_user
